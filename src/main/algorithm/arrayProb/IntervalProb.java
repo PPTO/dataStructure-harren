67b37129c154ec4fd795fd40e9c4d6eecd0d31a1
kris <1034879590@qq.com>
2023-08-24 12:53:55 +0800

    提交几道 链表 题

--- src/main/algorithm/arrayProb/IntervalProb.java
+++ src/main/algorithm/arrayProb/IntervalProb.java
@@ -51,16 +51,47 @@ public class IntervalProb {
      * Leecode 986. 区间列表的交集
      */
     public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
-
-        return null;
+        int i = 0, j = 0;
+        ArrayList<int[]> ans = new ArrayList<>();
+        while (i < firstList.length && j < secondList.length){
+            int min_right = Math.min(firstList[i][1], secondList[j][1]);
+            int max_left = Math.max(firstList[i][0], secondList[j][0]);
+            if (min_right >= max_left){
+                ans.add(new int[]{max_left,min_right});
+            }
+            // 更新
+            if (min_right == firstList[i][1])
+                i++;
+            else
+                j++;
+        }
+        return ans.toArray(new int[ans.size()][]);
     }
 
     /**
      * Leecode 452. 用最少数量的箭引爆气球
      */
     public int findMinArrowShots(int[][] points) {
-        // 排序
-
-        return -1;
+        // 排序 + 找公共区域
+        /**
+         * 特殊情况：当t1=-2147483645,t2=2147483647时。t1-t2发生越界。结果为+.
+         * return o1[0] - o2[0] (wrong)
+         */
+        Arrays.sort(points, (o1, o2) -> {
+            return o1[0] > o2[0] ? 1: -1;
+        });
+        int r = points[0][1];
+        int ans = 1;
+        for (int i = 1; i < points.length; i++) {
+            if (points[i][0] <= r){
+                // 公共区域更新
+                r = Math.min(points[i][1], r);
+            }
+            else {
+                r = points[i][1];
+                ans++;
+            }
+        }
+        return ans;
     }
 }
